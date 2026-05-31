0639f72661a34780a2cf317619d76bb067fa810a
alikondrr <alina.kondr.09@mail.ru>
2026-05-31 17:22:42 +0300

    Update OnlineGame.cs

--- Model/Core/OnlineGame.cs
+++ Model/Core/OnlineGame.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +6,28 @@ using System.Threading.Tasks;
 
 namespace Model.Core
 {
-  public class OnlineGame : Game, IComputerable, IMobile
-  {
-    public OnlineGame(string title, string genre, int ageRestriction,
-                     DateTime releaseDate, double qualityRating)
-        : base(title, genre, ageRestriction, releaseDate, qualityRating)
+    public class OnlineGame : Game, IComputerable, IMobile
     {
-    }
+        public OnlineGame(string title, string genre, int ageRestriction,
+                         DateTime releaseDate, double qualityRating)
+            : base(title, genre, ageRestriction, releaseDate, qualityRating)
+        {
+        }
 
-    public void PlayOnPC() { }
-    public void PlayOnMobile() { }
+        // Переопределение свойства (4)
+        public override double QualityRating
+        {
+            get => Math.Round(base.QualityRating * 0.95, 1);
+            set => base.QualityRating = value;
+        }
 
-    public override string GetGameInfo()
-    {
-      return $"[Online] {base.GetGameInfo()}";
+        public void PlayOnPC() { }
+        public void PlayOnMobile() { }
+
+        // Переопределение 3
+        public override string GetGameInfo()
+        {
+            return $"[Online] {base.GetGameInfo()}";
+        }
     }
-  }
 }
